6d8840d5b5e78cb05458a8fa041fc3ebe6874505
ssnirtek <ekaterinasysoeva3085@gmail.com>
2024-09-28 12:08:16 +0300

    ghghghhgl,jkuou

--- src/Main.kt
+++ src/Main.kt
@@ -3,11 +3,11 @@ fun main() {
 
         try {
             println("Введите строку:")
-            val input = readLine()?.trim()
+            val input = readLine()?.trim()// трим убирает пробелы в начале и конце строки
 
             val result = input?.let { countConsecutiveChars(it) }
             println("Результат: $result")
-        } catch (e: IllegalArgumentException) {
+        } catch (e: IllegalArgumentException) { //обработать ошибку
             println("Ошибка: ${e.message}")
         }
         finally {
@@ -24,22 +24,25 @@ fun countConsecutiveChars(str: String): String {
 
     for (i in 1 until str.length) {
         if (str[i] == currentChar && currentChar != ' ') {
+            //Проверяет, равен ли текущий символ символу. проверяет, что символ не пробел.
             count++
         } else {
             appendCharInfo(result, currentChar, count)
+            //Вызывает функцию, которая добавляет информацию о текущем символе и его количестве в результат.
             currentChar = str[i]
             count = 1
         }
     }
 
-    appendCharInfo(result, currentChar, count)
+    appendCharInfo(result, currentChar, count)// инф о последн символе
 
     return result.toString()
 }
 
 private fun appendCharInfo(builder: StringBuilder, char: Char, count: Int) {
+    // добавл инф о символе в результат
     builder.append(char)
-    if (count > 1 && char != ' ') {
-        builder.append(count)
+    if (count > 1 && char != ' ') {// если символ выполн больше 1 раза=>
+        builder.append(count)//добавл кол-во повторений
     }
 }
